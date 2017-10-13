e5c6561dbe5689551c2bc2eb3bdd9309a4ac5564
Travis CI User <travis@example.org>
2017-10-13 04:04:23 +0000

    Update documentation

---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hubcaps"] = ["impl&lt;'a, D, I&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"hubcaps/struct.Iter.html\" title=\"struct hubcaps::Iter\">Iter</a>&lt;'a, D, I&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;D: <a class=\"trait\" href=\"https://docs.rs/serde/1.0.15/serde/de/trait.DeserializeOwned.html\" title=\"trait serde::de::DeserializeOwned\">DeserializeOwned</a>,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
